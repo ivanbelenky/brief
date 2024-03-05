3983ee4e16e8bfde8d37632ce5f75ae35cc8b542
ivanbelenky <ivandietert@gmail.com>
2024-03-05 19:55:16 -0300

    no-meta

--- build_meta.py
+++ build_meta.py
@@ -1,10 +1,14 @@
 import os
 from datetime import datetime
-import sys
 
-def build_meta():
-    commit_message = sys.argv[1]
-    category, visible = commit_message.split(',')
+
+def build_meta(commit_message: str) -> int:
+    if commit_message.startswith('no-meta'): return 0
+    try: category, visible = commit_message.split(',')
+    except ValueError:
+        print("Invalid commit message. Expected format: 'category,visible' or 'no-meta'"
+              " if unrelated to new article")
+        return 1
     visible = int(visible)
     print(f"category: {category}, visible: {visible}")
     briefings = os.listdir('briefings')
@@ -16,6 +20,16 @@ def build_meta():
     for briefing_meta in briefings_meta:
         if briefing_meta not in briefings:
             os.remove(f'briefings/.meta/{briefing_meta}')
+    return 0
+
+def install_hook():
+    hook = """#!/bin/sh\npython3 build_meta.py "$1"
+"""
+    with open('.git/hooks/prepare-commit-msg', 'w') as f:
+        f.write(hook)
+    os.system('chmod +x .git/hooks/prepare-commit-msg')
 
 
-if __name__ == "__main__": build_meta()
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) > 1 and sys.argv[1] == 'install': install_hook()
